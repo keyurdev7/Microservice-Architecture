32634ab4033276d48ad71238c77b60935a6d21f3
keyurdev7 <60444224+keyurdev7@users.noreply.github.com>
2023-03-09 20:56:44 +0530

    script

---
+++ Test.API/DBScript/DBScript.sql
@@ -0,0 +1,149 @@
+USE [master]
+GO
+/****** Object:  Database [BlogManagement]    Script Date: 09-03-2023 20:50:55 ******/
+CREATE DATABASE [BlogManagement]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'BlogManagement', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\BlogManagement.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'BlogManagement_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\BlogManagement_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT
+GO
+ALTER DATABASE [BlogManagement] SET COMPATIBILITY_LEVEL = 150
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [BlogManagement].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [BlogManagement] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [BlogManagement] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [BlogManagement] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [BlogManagement] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [BlogManagement] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [BlogManagement] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [BlogManagement] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [BlogManagement] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [BlogManagement] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [BlogManagement] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [BlogManagement] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [BlogManagement] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [BlogManagement] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [BlogManagement] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [BlogManagement] SET  DISABLE_BROKER 
+GO
+ALTER DATABASE [BlogManagement] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [BlogManagement] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [BlogManagement] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [BlogManagement] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [BlogManagement] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [BlogManagement] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [BlogManagement] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [BlogManagement] SET RECOVERY FULL 
+GO
+ALTER DATABASE [BlogManagement] SET  MULTI_USER 
+GO
+ALTER DATABASE [BlogManagement] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [BlogManagement] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [BlogManagement] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [BlogManagement] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [BlogManagement] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [BlogManagement] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+EXEC sys.sp_db_vardecimal_storage_format N'BlogManagement', N'ON'
+GO
+ALTER DATABASE [BlogManagement] SET QUERY_STORE = OFF
+GO
+USE [BlogManagement]
+GO
+/****** Object:  Table [dbo].[Blog]    Script Date: 09-03-2023 20:50:55 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Blog](
+	[BlogId] [bigint] IDENTITY(1,1) NOT NULL,
+	[BlogTitle] [nvarchar](500) NOT NULL,
+	[BlogDescription] [nvarchar](max) NOT NULL,
+	[publishedDate] [datetime] NOT NULL,
+	[CreatedUserId] [bigint] NOT NULL,
+	[CreatedDate] [datetime] NOT NULL,
+	[IsDeleted] [bit] NOT NULL,
+ CONSTRAINT [PK_Blog] PRIMARY KEY CLUSTERED 
+(
+	[BlogId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[User]    Script Date: 09-03-2023 20:50:55 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[User](
+	[UserId] [bigint] IDENTITY(1,1) NOT NULL,
+	[UserName] [varchar](50) NOT NULL,
+	[Password] [varchar](50) NOT NULL,
+	[FirstName] [varchar](50) NOT NULL,
+	[LastName] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+SET IDENTITY_INSERT [dbo].[Blog] ON 
+GO
+INSERT [dbo].[Blog] ([BlogId], [BlogTitle], [BlogDescription], [publishedDate], [CreatedUserId], [CreatedDate], [IsDeleted]) VALUES (3, N'test3', N'des3', CAST(N'2023-03-09T12:08:06.200' AS DateTime), 1, CAST(N'2023-03-09T12:08:06.200' AS DateTime), 1)
+GO
+INSERT [dbo].[Blog] ([BlogId], [BlogTitle], [BlogDescription], [publishedDate], [CreatedUserId], [CreatedDate], [IsDeleted]) VALUES (7, N'Test1', N'test11', CAST(N'2023-03-09T14:05:33.483' AS DateTime), 1, CAST(N'2023-03-09T14:05:33.483' AS DateTime), 0)
+GO
+INSERT [dbo].[Blog] ([BlogId], [BlogTitle], [BlogDescription], [publishedDate], [CreatedUserId], [CreatedDate], [IsDeleted]) VALUES (8, N'Test2', N'test22', CAST(N'2023-03-09T14:05:33.483' AS DateTime), 1, CAST(N'2023-03-09T14:05:33.483' AS DateTime), 0)
+GO
+INSERT [dbo].[Blog] ([BlogId], [BlogTitle], [BlogDescription], [publishedDate], [CreatedUserId], [CreatedDate], [IsDeleted]) VALUES (9, N'Test6', N'test66', CAST(N'2023-03-09T14:05:33.483' AS DateTime), 1, CAST(N'2023-03-09T14:05:33.483' AS DateTime), 0)
+GO
+SET IDENTITY_INSERT [dbo].[Blog] OFF
+GO
+SET IDENTITY_INSERT [dbo].[User] ON 
+GO
+INSERT [dbo].[User] ([UserId], [UserName], [Password], [FirstName], [LastName]) VALUES (1, N'U1', N'U1', N'F1', N'F1')
+GO
+SET IDENTITY_INSERT [dbo].[User] OFF
+GO
+ALTER TABLE [dbo].[Blog] ADD  CONSTRAINT [DF_Blog_IsDeleted]  DEFAULT ((0)) FOR [IsDeleted]
+GO
+ALTER TABLE [dbo].[Blog]  WITH CHECK ADD  CONSTRAINT [FK_Blog_User] FOREIGN KEY([CreatedUserId])
+REFERENCES [dbo].[User] ([UserId])
+GO
+ALTER TABLE [dbo].[Blog] CHECK CONSTRAINT [FK_Blog_User]
+GO
+USE [master]
+GO
+ALTER DATABASE [BlogManagement] SET  READ_WRITE 
+GO
